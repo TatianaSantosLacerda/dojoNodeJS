24c28d3772acad64bdf68f7e15ef9c7a0ea67c76
Tatiana <tatianasantos2542@gmail.com>
2023-06-05 15:56:22 -0300

    implementado soma

---
+++ src/calculadora.js
@@ -0,0 +1,3 @@
+const soma = (numero1, numero2) => Number(numero1) + Number(numero2)
+
+export { soma }
---
+++ src/calculadora.test.js
@@ -0,0 +1,20 @@
+import {soma} from './calculadora'
+
+test('soma dois valores positivos', () => {
+    const resultado = soma(1,3)
+
+    expect(resultado).toBe(4)
+})
+
+test('soma dois valores passados como texto', () => {
+    const resultado = soma('1', '3')
+
+    expect(resultado).toBe(4)
+})
+
+
+test('soma valores invalidos', () => {
+    const resultado = soma('1', 'k')
+
+    expect(resultado).toBe(NaN)
+})
