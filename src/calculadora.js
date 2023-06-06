281df4044dd425d86bd2540d97349f1ecc1d9bae
Tatiana <tatianasantos2542@gmail.com>
2023-06-06 15:51:22 -0300

    versão com multiplicação

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,4 +2,6 @@ const soma = (numero1, numero2) => Number(numero1) + Number(numero2)
 
 const subtrai = (numero1, numero2) => Number(numero1) - Number(numero2)
 
-export { soma, subtrai }
+const multiplica = (numero1, numero2) => Number(numero1) * Number(numero2)
+
+export { soma, subtrai, multiplica }
